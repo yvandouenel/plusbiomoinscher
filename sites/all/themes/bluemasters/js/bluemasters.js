cef79836af3c6c7c53c9c35edf88c7fc995ef425
yvandouenel <yvan@coopernet.fr>
2015-07-31 12:14:13 +0200

    Ajout ce code pour régler le pb d'affichage du diaporama avec firefox.

--- sites/all/themes/bluemasters/js/bluemasters.js
+++ sites/all/themes/bluemasters/js/bluemasters.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function($) {	
+  /* Ajout ce code pour régler le pb d'affichage du diaporama avec firefox */
+  $(window).resize(function() {
+  var ww = $(window).width();
+  $('.flexslider ul li').each(function() {
+    $(this).width(ww);
+  });
+});
 
 	//Set Default State of each portfolio piece
 	$(".paging").show();
