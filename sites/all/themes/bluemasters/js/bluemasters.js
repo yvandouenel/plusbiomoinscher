ffe92e59508d6e6f1a99f92d87bb9cf5ecfaede4
yvandouenel <yvan@coopernet.fr>
2015-07-31 12:17:12 +0200

    Ajout ce code pour régler le pb d'affichage du diaporama avec firefox.

--- sites/all/themes/bluemasters/js/bluemasters.js
+++ sites/all/themes/bluemasters/js/bluemasters.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {	
   /* Ajout ce code pour régler le pb d'affichage du diaporama avec firefox */
+   console.log("Avant resize");
   $(window).resize(function() {
+    console.log("Dans resize");
   var ww = $(window).width();
   $('.flexslider ul li').each(function() {
     $(this).width(ww);
